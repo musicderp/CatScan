3cf13beaad7bdb968d4e3dddc3fd32c771cdcd50
Ben <benjaminmhuth@gmail.com>
2020-03-07 16:01:50 -0700

    Implemented text detection in cat pictures

--- TextDetection.py
+++ TextDetection.py
@@ -3,86 +3,77 @@ import numpy as np
 import argparse
 import time
 import cv2
+import os
+# for every file in directory, does text detection. if text is found, image deleted. if text is not, moved to /images/processed/
+directory = r'images/raw'
+for filename in os.listdir(directory):
+    path = os.path.join(directory, filename)
 
-image = cv2.imread("images/raw/yeet.jpg")
-orig = image.copy()
-(H, W) = image.shape[:2]
+    image = cv2.imread(path)
+    orig = image.copy()
+    (H, W) = image.shape[:2]
 
-(newW, newH) = (320, 320)
-rW = W / float(newW)
-rH = H / float(newH)
+    (newW, newH) = (320, 320)
+    rW = W / float(newW)
+    rH = H / float(newH)
 
-image = cv2.resize(image, (newW, newH))
-(H, W) = image.shape[:2]
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
 
-layerNames = [
-    "feature_fusion/Conv_7/Sigmoid",
-    "feature_fusion/concat_3"]
+    layerNames = [
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"]
 
+    print("[INFO] loading EAST text detector...")
+    net = cv2.dnn.readNet("frozen_east_text_detection.pb")
 
-print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+                                 (123.68, 116.78, 103.94), swapRB=True, crop=False)
+    start = time.time()
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+    end = time.time()
 
-blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
-                             (123.68, 116.78, 103.94), swapRB=True, crop=False)
-start = time.time()
-net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
-end = time.time()
+    print("[INFO] text detection took {:.6f} seconds".format(end - start))
 
-print("[INFO] text detection took {:.6f} seconds".format(end - start))
+    (numRows, numCols) = scores.shape[2:4]
+    rects = []
+    confidences = []
+    print(confidences)
 
+    for y in range(0, numRows):
 
-(numRows, numCols) = scores.shape[2:4]
-rects = []
-confidences = []
-print(confidences)
+        scoresData = scores[0, 0, y]
+        xData0 = geometry[0, 0, y]
+        xData1 = geometry[0, 1, y]
+        xData2 = geometry[0, 2, y]
+        xData3 = geometry[0, 3, y]
+        anglesData = geometry[0, 4, y]
 
-for y in range(0, numRows):
+        for x in range(0, numCols):
 
-    scoresData = scores[0, 0, y]
-    xData0 = geometry[0, 0, y]
-    xData1 = geometry[0, 1, y]
-    xData2 = geometry[0, 2, y]
-    xData3 = geometry[0, 3, y]
-    anglesData = geometry[0, 4, y]
+            if scoresData[x] < .5:
+                continue
 
+            (offsetX, offsetY) = (x * 4.0, y * 4.0)
 
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
 
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
 
-    for x in range(0, numCols):
+            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+            startX = int(endX - w)
+            startY = int(endY - h)
 
-        if scoresData[x] < .5:
-            continue
+            rects.append((startX, startY, endX, endY))
+            confidences.append(scoresData[x])
 
-        (offsetX, offsetY) = (x * 4.0, y * 4.0)
-
-        angle = anglesData[x]
-        cos = np.cos(angle)
-        sin = np.sin(angle)
-
-        h = xData0[x] + xData2[x]
-        w = xData1[x] + xData3[x]
-
-        endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-        endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-        startX = int(endX - w)
-        startY = int(endY - h)
-
-        rects.append((startX, startY, endX, endY))
-        confidences.append(scoresData[x])
-
-boxes = non_max_suppression(np.array(rects), probs=confidences)
-print(boxes)
-
-for (startX, startY, endX, endY) in boxes:
-
-    startX = int(startX * rW)
-    startY = int(startY * rH)
-    endX = int(endX * rW)
-    endY = int(endY * rH)
-
-    cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
-cv2.imshow("Text Detection", orig)
-cv2.waitKey(0)
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
+    if boxes == []:
+        os.rename(path, os.path.join("images/processed", filename))
+    else:
+        os.remove(path)
