fe2b9906f6faae86f20a8cc607f83fe2ec9761f9
Ben <benjaminmhuth@gmail.com>
2020-03-07 20:47:29 -0700

    Added first measure against image duplication

--- ImageGet.py
+++ ImageGet.py
@@ -1,46 +1,44 @@
+from io import open as iopen
 from urllib.parse import urlsplit
 
 import praw
-import os
-import time
 import requests
-from io import open as iopen
-
-
 
 reddit = praw.Reddit()
 
 subreddit = reddit.subreddit('blurrypicturesofcats')
-posts = subreddit.top(limit=10)
+posts = subreddit.new(limit=40)
+
+filename = "urls.txt"
+f = open(filename, 'r+')
+urlList = f.read().splitlines()
 
 for post in posts:
-  print(post.url)
-  file_url = post.url
-  if file_url[-3:] != "jpg":
-      print("Not an image")
+    print(post.url)
+    file_url = post.url
 
-  else:
-      suffix_list = ['jpg', 'gif', 'png', 'tif', 'svg', ]
-      file_name = urlsplit(file_url)[2].split('/')[-1]
-      file_suffix = file_name.split('.')[1]
-      print(file_name)
-      folderName = "images/raw/" + file_name
-      print(folderName)
+    if file_url[-3:] != "jpg":
+        print("Not an image")
 
-      i = requests.get(file_url)
-      if file_suffix in suffix_list and i.status_code == requests.codes.ok:
-          with iopen(folderName, 'wb') as file:
-              file.write(i.content)
-      else:
-          print("we goofed boys")
+    else:
+        if file_url in urlList:
+            print("Url has already been used")
+        else:
+            f.write(file_url + "\n")
 
+            urlList.append(file_url)
 
+            suffix_list = ['jpg', 'gif', 'png', 'tif', 'svg', ]
+            file_name = urlsplit(file_url)[2].split('/')[-1]
+            file_suffix = file_name.split('.')[1]
+            print(file_name)
+            folderName = "images/raw/" + file_name
+            print(folderName)
 
-
-
-
-
-
-
-
-
+            i = requests.get(file_url)
+            if file_suffix in suffix_list and i.status_code == requests.codes.ok:
+                with iopen(folderName, 'wb') as file:
+                    file.write(i.content)
+            else:
+                print("we goofed boys")
+f.close()
